d6b5eee3a32dbc5278a2e0de28f6aa7c23a42fd8
Salama A.B <devaksal@gmail.com>
2014-10-02 16:50:00 +0300

    CLean up; prep models

--- exemple/app.js
+++ exemple/app.js
@@ -22,6 +22,24 @@ app.get('/ipn_listener', PesaPal.parsePaymentNotification, function (req, res) {
     require('util').inspect(pesapal);
 });
 
+app.get('/checkout/:type(internal|external)', function (req, res, next) {
 
+    switch (req.params.type) {
+        case "internal":
+            // TODO: Show checkout UI, and post to PesaPal and parse the response
+
+            break;
+        case "external":
+            // TODO: Show checkout UI that will post to PesaPal
+
+
+            break;
+    }
+});
+
+
+app.post('/checkout', function (req, res, next) {
+    // TODO: Po
+});
 
 app.listen(3000);
